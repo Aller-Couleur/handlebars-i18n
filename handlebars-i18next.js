d3336626a914402ef69bc081275cb870baa40e19
Florian Walzel <fwalzel@gmx.de>
2020-03-11 11:59:46 +0100

    all basic functions are working

--- handlebars-i18next.js
+++ handlebars-i18next.js
@@ -12,6 +12,12 @@ var configuredOptions = {
       }
     };
 
+function __applyToConstructor(constructor, argArray) {
+  var args = [null].concat(argArray);
+  var factoryFunction = constructor.bind.apply(constructor, args);
+  return new factoryFunction();
+}
+
 module.exports = {
 
     /**
@@ -45,8 +51,9 @@ module.exports = {
     init : function() {
       handlebars.registerHelper('__',
         /**
-         * key as string retrieves the translation phrase
+         * retrieves the translation phrase from a key given as string
          * use like: {{__ "key_name"}}
+         * or with attributes: {{__ "key_with_count" count=7}}
          *
          * @param str
          * @param attributes
@@ -58,7 +65,7 @@ module.exports = {
       );
       handlebars.registerHelper('_v',
         /**
-         * the key comes from a handlebars variable instead
+         * retrieves the translation phrase from a key given as handlebars variable
          * use like: {{_v here.is.theKey}}
          *
          * @param str
@@ -71,7 +78,7 @@ module.exports = {
       );
       handlebars.registerHelper('_locale',
         /**
-         * echo the current language
+         * echos the current language
          * use like: {{_locale}}
          *
          * @returns {language|any|string|*|e}
@@ -82,7 +89,7 @@ module.exports = {
       );
       handlebars.registerHelper('localeIs',
         /**
-         * check against the current language
+         * checks against the current language
          * use like: {{#if (localeIs "en")}} Hello EN {{/if}}
          *
          * @returns {language|any|string|*|e}
@@ -93,43 +100,106 @@ module.exports = {
       );
       handlebars.registerHelper('_date',
           /**
+           * formats a given date by the give internationalization options
            *
-           * @param dateString
+           * allows multiple input forms:
+           * {{_date}}
+           * -> returns the current date
+           *
+           * {{_date "now" hour="numeric" minute="numeric" second="numeric"}}
+           * -> returns the current date with configured options
+           *
+           * {{_date 1583922952743}}
+           * -> returns the date given in milliseconds since begin of unix epoch
+           *
+           * {{_date "2020-03-11T03:24:00"}} or {{_date "March 11, 2020 03:24:00"}}
+           * -> returns the date given according to date string
+           *
+           * {{_date "[2020, 2, 11]"}}
+           * -> returns the date given according to parameters: year, month, day, hours, minutes, seconds, milliseconds
+           *
+           * @link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+           *
+           * @param dateInput
            * @param options
            */
-          function(dateString, options) {
-            let options =
-                configuredOptions.DateTimeFormat[i18next.language] ||
-                configuredOptions.DateTimeFormat.all || options;
-            const dateFormat = new Intl.DateTimeFormat(i18next.language, options);
-            return dateFormat.format(dateString);
+          function(dateInput, options) {
+
+            var date;
+
+            if (typeof dateInput == 'number') {
+              // input as milliseconds since unix epoch, like: 1583922952743
+              date = new Date(dateInput);
+            }
+            if (typeof dateInput == 'string') {
+
+              if (dateInput.charAt(0) == '[' && dateInput.slice(-1) == ']') {
+                // input as array represented as string such as "[2020, 11]"
+                dateInput = dateInput.substring(1, dateInput.length-1).replace(/ /g,'');
+                var dateArr = dateInput.split(',');
+                var dateFactory = __applyToConstructor.bind(null, Date);
+                date = dateFactory(dateArr);
+              }
+              else if (dateInput.toLowerCase() == 'now' || dateInput.toLowerCase() == 'today') {
+                // input as word "now" or "today"
+                date = new Date();
+              }
+              else {
+                // input as date string such as "1995-12-17T03:24:00"
+                date = new Date(dateInput);
+              }
+            }
+            else {
+              // fallback: today date
+              date = new Date();
+            }
+
+            var opts =
+              (typeof options !== 'undefined' && Object.keys(options.hash).length != 0) ?
+                options.hash : configuredOptions.DateTimeFormat[i18next.language] ||
+                configuredOptions.DateTimeFormat.all;
+
+            const dateFormat = new Intl.DateTimeFormat(i18next.language, opts);
+            return dateFormat.format(date);
           }
       );
       handlebars.registerHelper('_num',
           /**
+           * formats a given number by internationalization options
+           *
+           * use with preset: {{_num 3000}}
+           * or with individual option parameters: {{_num 3000 maximumSignificantDigits=1}}
+           *
+           *  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
            *
            * @param number
            * @param options
            * @returns {*}
            */
           function(number, options) {
-            let opts =
-                (typeof options === 'string') ? { style: 'currency', currency: currency } : null ||
-                configuredOptions.NumberFormat[i18next.language] ||
-                configuredOptions.NumberFormat.all || options;
+            var opts =
+                (Object.keys(options.hash).length != 0) ? options.hash : configuredOptions.NumberFormat[i18next.language] ||
+                configuredOptions.NumberFormat.all;
+
             const priceFormat = new intl.NumberFormat(i18next.language, opts);
             return priceFormat.format(number);
         }
       );
       handlebars.registerHelper('_price',
           /**
+           * formats a number as currency
+           *
+           * use with preset: {{_price 4999.99}
+           * or with individual option parameters: {{_price 4999.99 currency="EUR" maximumSignificantDigits=2}}
+           *
+           * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
            *
            * @param price
            * @param options
            * @returns {*}
            */
           function(price, options) {
-            let opts =
+            var opts =
                 (Object.keys(options.hash).length != 0) ? options.hash : configuredOptions.PriceFormat[i18next.language] ||
                 configuredOptions.PriceFormat.all;
 
