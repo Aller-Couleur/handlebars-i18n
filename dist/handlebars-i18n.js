843f588b55549b55c00a056b7e780a082247503c
FloWa <fwalzel@gmx.de>
2022-10-13 14:11:44 +0200

    chore/test: added a test to new init() function

--- dist/handlebars-i18n.js
+++ dist/handlebars-i18n.js
@@ -265,16 +265,17 @@
      */
     init: function (overrideHndlbrs, overrideI18n) {
 
-      if (typeof overrideHndlbrs === 'object')
+      if (typeof overrideHndlbrs === 'object' && overrideHndlbrs !== null)
         handlebars = overrideHndlbrs;
       else if (typeof overrideHndlbrs !== 'undefined' && overrideHndlbrs !== null)
-        console.error('@ handlebars-i18n.init(): Invalid Argument given for overrideHndlbrs. ' +
+        console.error('@ handlebars-i18n.init(): Invalid Argument [1] given for overrideHndlbrs. ' +
           'Argument must be the Handlebars Object. Using handlebars object on module instead.');
 
-      if (typeof overrideI18n === 'object')
+      if (typeof overrideI18n === 'object' && overrideI18n !== null)
         i18next = overrideI18n;
+
       else if (typeof overrideI18n !== 'undefined' && overrideI18n !== null)
-        console.error('@ handlebars-i18n.init(): Invalid Argument given for overrideI18n. ' +
+        console.error('@ handlebars-i18n.init(): Invalid Argument [2] given for overrideI18n. ' +
           'Argument must be the i18next Object. Using i18next object on module level instead.');
 
       handlebars.registerHelper('__',
