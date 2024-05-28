3e9b1d46dd61cdf97fcbedf692ed11ea1542348c
FloWa <fwalzel@gmx.de>
2024-05-28 09:22:00 +0200

    feat/refactor: added _dateRel and _dateDiff to simple-example.js, made improvements for clearer logging

--- examples/node-example/simple-example.js
+++ examples/node-example/simple-example.js
@@ -14,6 +14,7 @@
 const Handlebars = require('handlebars');
 const i18next = require('i18next');
 const HandlebarsI18n = require('../../dist/handlebars-i18n.js');
+const htmlEntities = require('html-entities');
 
 // -- The translation phrases for i18next
 i18next
@@ -72,101 +73,155 @@ HandlebarsI18n.configure([
   ['en', 'DateTimeFormat', { year:'numeric', month:'numeric', day:'numeric' }, 'custom-date-short'],
   ['de', 'DateTimeFormat', { year:'numeric', month:'numeric', day:'numeric' }, 'custom-date-short'],
   ['en', 'DateTimeFormat', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false}, 'custom-time'],
-  ['de', 'DateTimeFormat', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false}, 'custom-time']
+  ['de', 'DateTimeFormat', { hour:'numeric', minute:'numeric', second:'numeric', hour12:false}, 'custom-time'],
+  // custom formats for relative dates:
+  ['en', 'RelativeTimeFormat', { style: 'short', unit: 'year' }, 'date-rel-spec'],
+  ['de', 'RelativeTimeFormat', { style: 'short', unit: 'year' }, 'date-rel-spec']
 ]);
 
 let template;
 
-template = '\n' + 'EXAMPLE OUTPUT:' + '\n';
-template += '------------------------' + '\n';
+/**
+ * Replaces curly brackets by html entities so that it does not compile as handlebars.
+ * Also adds line breaks and an arrow symbol.
+ * Will be re-transformed with htmlEntities.decode() before logging
+ * @param str
+ * @returns {string}
+ */
+const raw = (str) => '\n\n' + str.replace(/{/g, '&lcub;').replace(/}/g, '&rcub;') + '\n&nbsp&#x2192;&nbsp;&nbsp;';
+
+template = '\n' + 'HandlebarsI18n example output:' + '\n';
+template += '-------------------------------------';
 
 // Translations
 // ----------------------------------------
 
 // Simple translation, key given as string:
-template += '{{__ "key1"}}' + '\n';
+template += raw('{{__ "key1"}}');
+template += '{{__ "key1"}}';
 
 // Translation with variable replacement:
-template += '{{__ "key2" what=sayWhat}}' + '\n';
+template += raw('{{__ "key2" what=sayWhat}}');
+template += '{{__ "key2" what=sayWhat}}';
 
 // Phrase with [singular] / plural:
-template += '{{__ "key3WithCount" count=1}}' + '\n';
+template += raw('{{__ "key3WithCount" count=1}}');
+template += '{{__ "key3WithCount" count=1}}';
 
 // Phrase with singular / [plural]:
-template += '{{__ "key3WithCount" count=7}}' + '\n';
+template += raw('{{__ "key3WithCount" count=7}}');
+template += '{{__ "key3WithCount" count=7}}';
 
 // Override language to use:
-template += '{{__ "key1" lng="de"}}' + '\n';
+template += raw('{{__ "key1" lng="de"}}');
+template += '{{__ "key1" lng="de"}}';
 
 
 // Output selected language
 // ----------------------------------------
 
 // Translation key given through handlebars variable and _locale output:
-template += '{{__ holdKey4}} {{_locale}}' + '\n';
+template += raw('{{__ holdKey4}} {{_locale}}');
+template += '{{__ holdKey4}} {{_locale}}';
 
 // Check against selected language:
-template += '{{#if (localeIs "en")}}English {{else}}Deutsch {{/if}}' + '\n';
+template += raw('{{#if (localeIs "en")}}English {{else}}Deutsch {{/if}}');
+template += '{{#if (localeIs "en")}}English {{else}}Deutsch {{/if}}';
 
 
 // Number representation
 // ----------------------------------------
 
 // Number representation as configured for all languages:
-template += '{{_num 7000}}' + '\n';
+template += raw('{{_num 7000}}');
+template += '{{_num 7000}}';
 
 // Number representation with specific format attribute:
-template += '{{_num 3.1415926 maximumFractionDigits=0}}' + '\n';
+template += raw('{{_num 3.1415926 maximumFractionDigits=0}}');
+template += '{{_num 3.1415926 maximumFractionDigits=0}}';
 
 // Number and attribute given through handlebars variables:
-template += '{{_num mynumber maximumFractionDigits=myMaxDigits}}' + '\n';
+template += raw('{{_num mynumber maximumFractionDigits=myMaxDigits}}');
+template += '{{_num mynumber maximumFractionDigits=myMaxDigits}}';
 
 
 // Price representation
 // ----------------------------------------
 
 // Price representation as configured per language:
-template += '{{_price 9999.99}}' + '\n';
+template += raw('{{_price 9999.99}}');
+template += '{{_price 9999.99}}';
 
 // Price representation with specific format attributes:
-template += '{{_price 1000.99 currency="JPY" minimumFractionDigits=0}}' + '\n';
+template += raw('{{_price 1000.99 currency="JPY" minimumFractionDigits=0}}');
+template += '{{_price 1000.99 currency="JPY" minimumFractionDigits=0}}';
 
-// Price given through handlebars variable and with with specific format attribute:
-template += '{{_price myPrice currency="DKK"}}' + '\n';
+// Price given through handlebars variable and with specific format attribute:
+template += raw('{{_price myPrice currency="DKK"}}');
+template += '{{_price myPrice currency="DKK"}}';
 
 
-// date representation
+// Date representation
 // ----------------------------------------
 
-// Todays date as configured per language:
-template += '{{_date}}' + '\n';
+// Todays’ date as configured per language:
+template += raw('{{_date}}');
+template += '{{_date}}';
 
 // Date given as date string:
-template += '{{_date "2020-03-11T03:24:00"}}' + '\n';
+template += raw('{{_date "2020-03-11T03:24:00"}}');
+template += '{{_date "2020-03-11T03:24:00"}}';
 
 // Date given in milliseconds since begin of unix epoch:
-template += '{{_date 1583922952743}}' + '\n';
+template += raw('{{_date 1583922952743}}');
+template += '{{_date 1583922952743}}';
 
 // Date given as javascript date parameter array:
-template += '{{_date "[2012, 11, 20, 3, 0, 0]"}}' + '\n';
+template += raw('{{_date "[2012, 11, 20, 3, 0, 0]"}}');
+template += '{{_date "[2012, 11, 20, 3, 0, 0]"}}';
 
-// Todays date with with specific format attributes:
-template += '{{_date "today" year="2-digit" month="2-digit" day="2-digit"}}' + '\n';
+// Todays’ date with specific format attributes:
+template += raw('{{_date "today" year="2-digit" month="2-digit" day="2-digit"}}');
+template += '{{_date "today" year="2-digit" month="2-digit" day="2-digit"}}';
 
 // Date given through handlebars variable:
-template += '{{_date myDate}}' + '\n';
+template += raw('{{_date myDate}}');
+template += '{{_date myDate}}';
 
-// Date formated by custom configuration (subset "custom-year-only"):
-template += '{{_date myDate format="custom-year-only"}}' + '\n';
+// Date formatted by custom configuration (subset "custom-year-only"):
+template += raw('{{_date myDate format="custom-year-only"}}');
+template += '{{_date myDate format="custom-year-only"}}';
 
-// Date formated by custom configuration (subset "custom-date-short"):
-template += '{{_date myDate format="custom-date-short"}}' + '\n';
+// Date formatted by custom configuration (subset "custom-date-short"):
+template += raw('{{_date myDate format="custom-date-short"}}');
+template += '{{_date myDate format="custom-date-short"}}';
 
-// Date formated by custom configuration (subset "custom-date-short"):
-template += '{{_date myDate format="custom-time"}}' + '\n';
+// Date formatted by custom configuration (subset "custom-date-short"):
+template += raw('{{_date myDate format="custom-time"}}');
+template += '{{_date myDate format="custom-time"}}';
+
+// Relative date representation
+// ----------------------------------------
+
+// Relative date event in the future:
+template += raw('{{_dateRel 7 unit="hour"}}');
+template += '{{_dateRel 7 unit="hour"}}';
+
+// Relative date event in the past:
+template += raw('{{_dateRel -7 unit="hour"}}');
+template += '{{_dateRel -7 unit="hour"}}';
+
+// Date difference:
+template += raw('{{_dateDiff "1996-12-07T00:00:00" "1996-12-08T00:00:00" unit="day"}}');
+template += '{{_dateDiff "1996-12-07T00:00:00" "1996-12-08T00:00:00" unit="day"}}';
+
+// Date difference by custom configuration (subset "date-rel-spec"):
+template += raw('{{_dateDiff mydate "1995-12-17T00:00:00" format="date-rel-spec"}}');
+template += '{{_dateDiff mydate "1995-12-17T00:00:00" format="date-rel-spec"}}';
 
 
-let compiled = Handlebars.compile(template);
+const compiled = Handlebars.compile(template);
 i18next.changeLanguage('de'); // --> Test the changes by replacing 'de' with 'en'
+const decoded = htmlEntities.decode(compiled(data));
 
-console.log('\x1b[36m%s\x1b[0m', compiled(data));
+console.log('\x1b[36m%s\x1b[0m', decoded);
